d2558d7a3e3d774e7dfe7101d5f71818e6a2580d
ibrahimyoruk4858@gmail.com <ibrahimyoruk4858@gmail.com>
2024-10-17 13:23:49 +0300

    browser and mytagram

---
+++ browser/app/src/main/java/gui/ceng/mu/edu/browser/MainActivity.java
@@ -0,0 +1,35 @@
+package gui.ceng.mu.edu.browser;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
+
+import androidx.activity.EdgeToEdge;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
--- browser/settings.gradle.kts
+++ browser/settings.gradle.kts
@@ -19,6 +19,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "MyTagram"
+rootProject.name = "browser"
 include(":app")
- 
